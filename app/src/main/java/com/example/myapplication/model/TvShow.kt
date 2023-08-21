10e4edc25c19cfe04aabed3027d5b5973cbd0c60
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-08-21 10:06:34 +0200

    Update TvShow.kt

--- app/src/main/java/com/example/myapplication/model/TvShow.kt
+++ app/src/main/java/com/example/myapplication/model/TvShow.kt
@@ -5,6 +5,7 @@ import java.io.Serializable
 class TvShow(val id: Int,
              val name: String,
              val year: Int,
+             val genre: String
              val rating:Double,
              val imageId:Int,
              val overview:String
